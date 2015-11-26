7c505e83c1cbd5ed0f1ce15a176a0e8b440c2328
Hannes Gräuler <hgraeule@uos.de>
2015-11-26 17:47:52 +0100

    Add BinaryStorageConnection class

--- syncrypt/backends/binary.py
+++ syncrypt/backends/binary.py
@@ -5,6 +5,35 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+class BinaryStorageConnection(object):
+    def __init__(self, storage):
+        self.storage = storage
+
+    @asyncio.coroutine
+    def __enter__(self):
+        self.reader, self.writer = \
+                yield from asyncio.open_connection(self.storage.host,
+                                                   self.storage.port)
+
+        self.writer.write('AUTH:{0}\r\n'
+                .format(self.storage.auth)
+                .encode(self.storage.vault.config.encoding))
+        yield from self.writer.drain()
+
+        line = yield from self.reader.readline()
+        if line != b'SUCCESS\r\n':
+            raise Exception(line)
+
+        return self
+
+    def __exit__(self, *args):
+
+        self.writer.write(b'DISCONNECT\r\n')
+        yield from self.writer.drain()
+
+        self.writer.close()
+
+
 class BinaryStorageBackend(StorageBackend):
 
     def __init__(self, vault, auth='foo', host='127.0.0.1', port=1337, concurrency=4):
@@ -32,45 +61,34 @@ class BinaryStorageBackend(StorageBackend):
     def _upload(self, bundle):
 
         logger.info('Uploading %s', bundle)
-        reader, writer = \
-                yield from asyncio.open_connection(self.host, self.port)
 
-        writer.write('AUTH:{0}\r\n'
-                .format(self.auth)
-                .encode(self.vault.config.encoding))
-        yield from writer.drain()
+        with BinaryStorageConnection(self) as conn:
+            conn = yield from conn
 
-        line = yield from reader.readline()
-        if line != b'SUCCESS\r\n':
-            raise Exception(line)
+            # upload key and file
+            conn.writer.write('UPLOAD:{0.store_hash}:{0.key_size_crypt}:{0.file_size_crypt}\r\n'
+                    .format(bundle)
+                    .encode(self.vault.config.encoding))
+            yield from conn.writer.drain()
 
-        # upload key and file
-        writer.write('UPLOAD:{0.store_hash}:{0.key_size_crypt}:{0.file_size_crypt}\r\n'
-                .format(bundle)
-                .encode(self.vault.config.encoding))
-        yield from writer.drain()
+            line = yield from conn.reader.readline()
+            if line != b'WAITING\r\n':
+                raise Exception(line)
 
-        line = yield from reader.readline()
-        if line != b'WAITING\r\n':
-            raise Exception(line)
+            with open(bundle.path_key, 'rb') as f:
+                while f.tell() < bundle.key_size_crypt:
+                    buf = f.read(self.buf_size)
+                    conn.writer.write(buf)
+                    yield from conn.writer.drain()
 
-        with open(bundle.path_key, 'rb') as f:
-            while f.tell() < bundle.key_size_crypt:
-                buf = f.read(self.buf_size)
-                writer.write(buf)
-                yield from writer.drain()
+            with open(bundle.path_crypt, 'rb') as f:
+                while f.tell() < bundle.file_size_crypt:
+                    buf = f.read(self.buf_size)
+                    conn.writer.write(buf)
+                    yield from conn.writer.drain()
 
-        with open(bundle.path_crypt, 'rb') as f:
-            while f.tell() < bundle.file_size_crypt:
-                buf = f.read(self.buf_size)
-                writer.write(buf)
-                yield from writer.drain()
+            line = yield from conn.reader.readline()
+            if line != b'SUCCESS\r\n':
+                raise Exception(line)
 
-        line = yield from reader.readline()
-        if line != b'SUCCESS\r\n':
-            raise Exception(line)
 
-        writer.write(b'DISCONNECT\r\n')
-        yield from writer.drain()
-
-        writer.close()
