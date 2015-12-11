9f2a25a43d4372dcef139b95fe6a94fafa4ab7f3
Hannes Gräuler <hgraeule@uos.de>
2015-12-11 22:10:52 +0100

    Add logging to unit tests

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,6 +1,11 @@
+import logging
 import unittest
+
 from test_binary import *
 from test_local import *
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
 
 if __name__ == '__main__':
     unittest.main()
