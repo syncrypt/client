034b43c37ac96df5f7491a80b0113a2a12e635e9
Hannes Gräuler <hgraeule@uos.de>
2015-12-20 19:36:06 +0100

    Do not pad if not needed

--- syncrypt/streams.py
+++ syncrypt/streams.py
@@ -11,8 +11,11 @@ logger = logging.getLogger(__name__)
 class PKCS5Padding(object):
     @staticmethod
     def pad(s, block_size):
-        return s + str.encode((block_size - len(s) % block_size) *
-                chr(block_size - len(s) % block_size))
+        if len(s) % block_size > 0:
+            return s + str.encode((block_size - len(s) % block_size) *
+                    chr(block_size - len(s) % block_size))
+        else:
+            return s
 
     @staticmethod
     def unpad(s):
