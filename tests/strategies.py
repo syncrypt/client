07959e8dafcb601c8a8dd5161eeefd53964cd562
Hannes Gräuler <hgraeule@uos.de>
2016-03-01 20:22:01 +0100

    Do not allow '.' or '..' in test filenames

--- tests/strategies.py
+++ tests/strategies.py
@@ -13,11 +13,13 @@ def file(draw, filename=st.lists(
     return {'filename': ''.join(draw(filename)),
             'content': draw(content)}
 
+valid_file = file().filter(lambda f: not f['filename'] in ('.', '..'))
+
 @st.composite
 def vault(draw):
     # TODO: use st.recursive to generate files in folders
     # TODO: filter duplicate filenames
-    return draw(st.lists(file(), average_size=5, max_size=MAX_FILES))
+    return draw(st.lists(valid_file, average_size=5, max_size=MAX_FILES))
 
 if __name__ == '__main__':
     print (vault().example())
