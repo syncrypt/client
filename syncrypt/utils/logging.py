f8150eaad96d82ef7894134081093ec6b771a06f
Hannes Gräuler <hgraeule@uos.de>
2016-03-01 20:52:04 +0100

    Make colorlog dependency explicit (for PyInstaller to find) (closes #12)

--- syncrypt/utils/logging.py
+++ syncrypt/utils/logging.py
@@ -1,11 +1,12 @@
 import logging.config
+import colorlog
 
 def setup_logging(loglevel):
     logging.config.dictConfig({
         'version': 1,
         'formatters': {
             'colored': {
-                '()': 'colorlog.ColoredFormatter',
+                '()': colorlog.ColoredFormatter,
                 'format':
                     '%(log_color)s%(levelname)s:%(name)s:%(message)s',
             }
