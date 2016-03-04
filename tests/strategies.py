13b9db94e162282734f77f8658d320d5d26bd177
Hannes Gräuler <hgraeule@uos.de>
2016-03-04 08:32:40 +0100

    Do not allow |, >, < in hypothesis file names

--- tests/strategies.py
+++ tests/strategies.py
@@ -7,7 +7,7 @@ MAX_FILES = 500
 @st.composite
 def file(draw, filename=st.lists(
         st.characters(blacklist_categories=('Cs', 'Cc'),
-            blacklist_characters=('\0\n\r/\\')),
+            blacklist_characters=('\0\n\r/\\|><')),
             min_size=1, average_size=20),
             content=st.binary(max_size=10000, average_size=100)):
     return {'filename': ''.join(draw(filename)),
@@ -15,11 +15,17 @@ def file(draw, filename=st.lists(
 
 valid_file = file().filter(lambda f: not f['filename'] in ('.', '..'))
 
+def has_no_duplicate(v):
+    return len(v) == len({f['filename'] for f in v})
+
 @st.composite
 def vault(draw):
     # TODO: use st.recursive to generate files in folders
     # TODO: filter duplicate filenames
-    return draw(st.lists(valid_file, average_size=5, max_size=MAX_FILES))
+    return draw(st.lists(valid_file, average_size=5, max_size=MAX_FILES)\
+            .filter(has_no_duplicate))
+
+#valid_vault = vault().filter(has_no_duplicate)
 
 if __name__ == '__main__':
     print (vault().example())
