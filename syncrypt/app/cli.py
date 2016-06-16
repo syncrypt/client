d0fba9b7189c2cd06e5bd35767a5c536a62f72af
Hannes Gräuler <hannes@smasi.de>
2016-06-16 18:57:13 +0200

    Simplify user input (non async)
    Fixes #35

--- syncrypt/app/cli.py
+++ syncrypt/app/cli.py
@@ -1,24 +1,6 @@
 from syncrypt.app.auth import AuthenticationProvider
 import asyncio
-import sys
 from getpass import getpass
-
-@asyncio.coroutine
-def stdio(loop):
-    reader = asyncio.StreamReader(loop=loop)
-    reader_protocol = asyncio.StreamReaderProtocol(reader)
-    try:
-        yield from loop.connect_read_pipe(lambda: reader_protocol, sys.stdin)
-    except ValueError as e:
-        raise e from None # hide exception stack
-    return reader
-
-@asyncio.coroutine
-def readline_from_stdin(password=False):
-    loop = asyncio.get_event_loop()
-    reader = yield from stdio(loop)
-    line = yield from reader.readline()
-    return line.decode().replace('\r', '').replace('\n', '')
 
 class CLIAuthenticationProvider(AuthenticationProvider):
 
@@ -26,7 +8,6 @@ class CLIAuthenticationProvider(AuthenticationProvider):
     def get_auth(self, backend):
         username = None
         while not username:
-            print('Email for {}: '.format(backend.host), end='', flush=True)
-            username = yield from readline_from_stdin()
+            username = input('Email for {}: '.format(backend.host))
         password = getpass()
         return username, password
