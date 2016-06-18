017940989f81e8869781f8627d00602d72d3c20c
Hannes Gräuler <hannes@smasi.de>
2016-06-18 18:56:53 +0200

    Version bump 0.1.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from os import path
 from setuptools import find_packages, setup
 
 __name__ = 'syncrypt'
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 
 here = path.abspath(path.dirname(__file__))
 
