cace4c2ea52fbba539878d05573d76442e00d1ea
Hannes Gräuler <hannes@smasi.de>
2016-08-28 13:54:13 +0200

    Version bump 0.2.0

--- syncrypt/__init__.py
+++ syncrypt/__init__.py
@@ -1,7 +1,7 @@
 from pkg_resources import get_distribution, DistributionNotFound
 
 __project__ = 'syncrypt'
-__version__ = '0.1.4'
+__version__ = '0.2.0'
 
 try:
     __version__ = get_distribution(__project__).version
