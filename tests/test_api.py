d695600bd7c2c63d1c47275dd2b80306dead24fd
Hannes Gräuler <hannes@smasi.de>
2016-09-12 08:33:29 +0200

    Refactor test endpoint into constant

--- tests/test_api.py
+++ tests/test_api.py
@@ -16,6 +16,7 @@ from syncrypt.config import AppConfig
 from syncrypt.models import Vault
 from tests.base import VaultTestCase, TestAppConfig
 
+TEST_API_ENDPOINT = 'http://127.0.0.1:28080'
 
 class APITests(VaultTestCase):
     folder = 'tests/testbinaryvault/'
@@ -29,22 +30,22 @@ class APITests(VaultTestCase):
                 'email': 'test@syncrypt.space',
                 'password': 'test!password'
             })
-            r = yield from aiohttp.post('http://127.0.0.1:28080/v1/auth/login/', data=login_data)
+            r = yield from aiohttp.post(TEST_API_ENDPOINT + '/v1/auth/login/', data=login_data)
             yield from r.release()
             self.assertEqual(r.status, 200)
 
-            r = yield from aiohttp.get('http://127.0.0.1:28080/v1/auth/check/')
+            r = yield from aiohttp.get(TEST_API_ENDPOINT + '/v1/auth/check/')
             c = yield from r.json()
             yield from r.release()
             self.assertEqual(r.status, 200)
             self.assertEqual(c['connected'], True)
 
-            r = yield from aiohttp.get('http://127.0.0.1:28080/v1/auth/logout/')
+            r = yield from aiohttp.get(TEST_API_ENDPOINT + '/v1/auth/logout/')
             c = yield from r.json()
             self.assertEqual(r.status, 200)
             yield from r.release()
 
-            r = yield from aiohttp.get('http://127.0.0.1:28080/v1/auth/check/')
+            r = yield from aiohttp.get(TEST_API_ENDPOINT + '/v1/auth/check/')
             c = yield from r.json()
             self.assertEqual(r.status, 200)
             yield from r.release()
@@ -59,18 +60,18 @@ class APITests(VaultTestCase):
         app.add_vault(self.vault)
         yield from app.start()
         try:
-            r = yield from aiohttp.get('http://127.0.0.1:28080/v1/vault/')
+            r = yield from aiohttp.get(TEST_API_ENDPOINT + '/v1/vault/')
             c = yield from r.json()
             self.assertEqual(len(c), 1) # only one vault
             yield from r.release()
 
             vault_uri = c[0]['resource_uri']
 
-            r = yield from aiohttp.get('http://127.0.0.1:28080%s' % vault_uri)
+            r = yield from aiohttp.get(TEST_API_ENDPOINT + '%s' % vault_uri)
             yield from r.release()
 
             # get a list of bundles
-            bundle_list_uri = 'http://127.0.0.1:28080%sbundle/' % vault_uri
+            bundle_list_uri = TEST_API_ENDPOINT + '%sbundle/' % vault_uri
 
             r = yield from aiohttp.get(bundle_list_uri)
             c = yield from r.json()
@@ -88,12 +89,12 @@ class APITests(VaultTestCase):
 
             yield from asyncio.sleep(0.1)
 
-            r = yield from aiohttp.get('http://127.0.0.1:28080/v1/vault/')
+            r = yield from aiohttp.get(TEST_API_ENDPOINT + '/v1/vault/')
             c = yield from r.json()
             self.assertEqual(len(c), 0) # no vault
             yield from r.release()
 
-            r = yield from aiohttp.post('http://127.0.0.1:28080/v1/vault/',
+            r = yield from aiohttp.post(TEST_API_ENDPOINT + '/v1/vault/',
                     data=json.dumps({ 'folder': self.vault.folder }))
             c = yield from r.json()
             self.assertGreater(len(c['resource_uri']), 5)
@@ -101,12 +102,12 @@ class APITests(VaultTestCase):
 
             teh_vault = c['resource_uri']
 
-            r = yield from aiohttp.get('http://127.0.0.1:28080/v1/vault/')
+            r = yield from aiohttp.get(TEST_API_ENDPOINT + '/v1/vault/')
             c = yield from r.json()
             self.assertEqual(len(c), 1) # one vault
             yield from r.release()
 
-            r = yield from aiohttp.get('http://127.0.0.1:28080' + teh_vault + 'users/')
+            r = yield from aiohttp.get(TEST_API_ENDPOINT + teh_vault + 'users/')
             c = yield from r.json()
 
             self.assertEqual(len(c), 1) # one user
@@ -115,21 +116,21 @@ class APITests(VaultTestCase):
             # add_user will make sure that the email is added as a user to the vault.
             # it will then return all the keys.
             # For testing purpose, we will add ourselves again
-            me = c[0]['email']
-            r = yield from aiohttp.post('http://127.0.0.1:28080' + teh_vault + 'users/',
-                data=json.dumps({'email': me}))
+            me = c[0]
+            r = yield from aiohttp.post(TEST_API_ENDPOINT + teh_vault + 'users/',
+                data=json.dumps({'email': me['email']}))
             c = yield from r.json()
             yield from r.release()
 
-            r = yield from aiohttp.get('http://127.0.0.1:28080' + teh_vault + 'users/' + me + '/keys/')
+            r = yield from aiohttp.get(TEST_API_ENDPOINT + teh_vault + 'users/' + me['email'] + '/keys/')
             c = yield from r.json()
             self.assertGreater(len(c), 0) # at least one key
             yield from r.release()
 
-            r = yield from aiohttp.delete('http://127.0.0.1:28080' + teh_vault)
+            r = yield from aiohttp.delete(TEST_API_ENDPOINT + teh_vault)
             yield from r.release()
 
-            r = yield from aiohttp.get('http://127.0.0.1:28080/v1/vault/')
+            r = yield from aiohttp.get(TEST_API_ENDPOINT + '/v1/vault/')
             c = yield from r.json()
             self.assertEqual(len(c), 0) # no vault
             yield from r.release()
@@ -147,12 +148,12 @@ class APITests(VaultTestCase):
 
         try:
 
-            r = yield from aiohttp.get('http://127.0.0.1:28080/v1/vault/')
+            r = yield from aiohttp.get(TEST_API_ENDPOINT + '/v1/vault/')
             c = yield from r.json()
             self.assertEqual(len(c), 1) # only one vault
             yield from r.release()
 
-            r = yield from aiohttp.get('http://127.0.0.1:28080/v1/stats')
+            r = yield from aiohttp.get(TEST_API_ENDPOINT + '/v1/stats')
             c = yield from r.json()
             self.assertEqual(c['stats']['downloads'], 0)
             self.assertEqual(c['stats']['uploads'], 8)
@@ -161,14 +162,14 @@ class APITests(VaultTestCase):
 
             yield from app.push()
 
-            r = yield from aiohttp.get('http://127.0.0.1:28080/v1/stats')
+            r = yield from aiohttp.get(TEST_API_ENDPOINT + '/v1/stats')
             c = yield from r.json()
             self.assertEqual(c['stats']['downloads'], 0)
             self.assertEqual(c['stats']['uploads'], 8)
             self.assertEqual(c['stats']['stats'], 16)
             yield from r.release()
 
-            r = yield from aiohttp.get('http://127.0.0.1:28080/v1/config')
+            r = yield from aiohttp.get(TEST_API_ENDPOINT + '/v1/config')
             c = yield from r.json()
             self.assertIn('api', c.keys())
             yield from r.release()
