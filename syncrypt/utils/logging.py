b3d36a60acd914a6695c1011b18fe32be5918f0b
Hannes Gräuler <graeuler@geoplex.de>
2016-10-19 20:45:36 +0200

    syncrypt_daemon writes DEBUG logs to file

--- syncrypt/utils/logging.py
+++ syncrypt/utils/logging.py
@@ -1,14 +1,18 @@
 import logging.config
 import colorlog
 
-def setup_logging(loglevel):
-    logging.config.dictConfig({
+def setup_logging(loglevel, logfile=None):
+    config = {
         'version': 1,
         'formatters': {
             'colored': {
                 '()': colorlog.ColoredFormatter,
                 'format':
                     '%(log_color)s[%(levelname)-8s] %(message)s',
+            },
+            'precise': {
+                'format': '%(asctime)s %(levelname)s [%(name)s] %(message)s',
+                'datefmt': '%Y-%m-%d %H:%M:%S'
             }
         },
         'handlers': {
@@ -28,4 +32,18 @@ def setup_logging(loglevel):
                 'level': loglevel
             },
         },
-    })
+    }
+    if logfile:
+        config['handlers']['file'] = {
+            'class': 'logging.handlers.TimedRotatingFileHandler',
+            'level': logging.DEBUG,
+            'filename': logfile,
+            'when': 'D',
+            'formatter': 'precise',
+            'backupCount': 30
+        }
+        config['loggers']['syncrypt']['handlers'].append('file')
+        config['loggers']['syncrypt']['level'] = logging.DEBUG
+        config['loggers']['aiohttp']['handlers'].append('file')
+        config['loggers']['aiohttp']['level'] = logging.DEBUG
+    logging.config.dictConfig(config)
