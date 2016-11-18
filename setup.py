579a74b4d79631866e66512761784fdf00c69186
Hannes Gräuler <graeuler@geoplex.de>
2016-11-18 09:24:36 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from os import path
 from setuptools import find_packages, setup
 
 __name__ = 'syncrypt'
-__version__ = '0.3.4'
+__version__ = '0.3.5'
 
 here = path.abspath(path.dirname(__file__))
 
