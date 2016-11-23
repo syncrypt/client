bbfb5671dc2d93f525683cefa57cbe173fb44c46
Hannes Gräuler <graeuler@geoplex.de>
2016-11-23 20:57:38 +0100

    Release 0.3.8

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from os import path
 from setuptools import find_packages, setup
 
 __name__ = 'syncrypt'
-__version__ = '0.3.7'
+__version__ = '0.3.8'
 
 here = path.abspath(path.dirname(__file__))
 
