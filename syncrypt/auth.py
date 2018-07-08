f40528f7b8f3a337a17a6caaa040707a67284ad3
Hannes Gräuler <hannes@smasi.de>
2018-07-08 13:09:53 +0200

    Add auth.py

---
+++ syncrypt/auth.py
@@ -0,0 +1,16 @@
+
+
+class AuthenticationProvider():
+
+    async def get_auth(self, backend):
+        raise NotImplementedError
+
+
+class CredentialsAuthenticationProvider(AuthenticationProvider):
+
+    def __init__(self, username, password):
+        self._username = username
+        self._password = password
+
+    async def get_auth(self, backend):
+        return self._username, self._password
