13228e9702345c5e9d877fc58a7cc2abb1137f93
Hannes Gräuler <hannes@smasi.de>
2018-11-14 17:30:52 +0100

    Add some type anns

--- syncrypt/managers/vault_user.py
+++ syncrypt/managers/vault_user.py
@@ -11,11 +11,11 @@ class VaultUserManager:
     def __init__(self, app):
         self.app = app
 
-    def add(self, vault, user_id):
+    def add(self, vault: Vault, user_id: str):
         with store.session() as session:
             session.add(VaultUser(vault_id=vault.id, user_id=user_id))
 
-    def remove(self, vault, user_id):
+    def remove(self, vault: Vault, user_id: str):
         with store.session() as session:
             return (
                 session
@@ -24,10 +24,10 @@ class VaultUserManager:
                     .delete()
             )
 
-    def list_for_vault(self, vault):
+    def list_for_vault(self, vault: Vault):
         with store.session() as session:
             return (
-                session.query(VaultUser)
+                session.query(self.model)
                 .filter(self.model.vault_id == vault.id)
                 .all()
             )
@@ -39,7 +39,7 @@ class VaultUserManager:
     def find_key(self, vault: Vault, fingerprint) -> VaultUser:
         with store.session() as session:
             return (
-                session.query(VaultUser)
+                session.query(self.model)
                 .filter(self.model.vault_id == vault.id)
                 .filter(self.model.fingerprint == fingerprint)
                 .first()
