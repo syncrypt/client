93a09debfc1f7285ab9cecc08bb47ed9624d5173
Hannes Gräuler <hannes@smasi.de>
2018-12-16 16:29:54 +0100

    Port hachiko fix for newer python versions

--- syncrypt/vendor/hachiko/hachiko.py
+++ syncrypt/vendor/hachiko/hachiko.py
@@ -23,6 +23,11 @@ class AIOEventHandler(object):
     async def on_modified(self, event): pass
 
     def dispatch(self, event):
+        if hasattr(asyncio, 'ensure_future'):
+            ensure_future = asyncio.ensure_future
+        else:  # deprecated since Python 3.4.4
+            ensure_future = getattr(asyncio, 'async')
+
         _method_map = {
             EVENT_TYPE_MODIFIED: self.on_modified,
             EVENT_TYPE_MOVED: self.on_moved,
@@ -32,7 +37,7 @@ class AIOEventHandler(object):
         handlers = [self.on_any_event, _method_map[event.event_type]]
         for handler in handlers:
             self._loop.call_soon_threadsafe(
-                getattr(asyncio, 'async'),
+                ensure_future,
                 handler(event))
 
 
