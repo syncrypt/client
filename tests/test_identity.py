303da5a6a4c4657d188033a1bf7f85a0592e157c
Hannes Gräuler <hannes@smasi.de>
2019-02-10 19:40:59 +0100

    Remove test with timing

--- tests/test_identity.py
+++ tests/test_identity.py
@@ -36,40 +36,6 @@ async def test_creation(working_dir):
 
 
 @pytest.mark.slow
-@pytest.mark.skip
-async def test_async_key_generation(working_dir):
-    "check wether key generation can happen concurrently"
-
-    key = os.path.join(working_dir, "id_rsa")
-    key_pub = os.path.join(working_dir, "id_rsa.pub")
-
-    if os.path.exists(key):
-        os.unlink(key)
-    if os.path.exists(key_pub):
-        os.unlink(key_pub)
-
-    identity = Identity(key, key_pub, AppConfig())
-
-    # loop = asyncio.get_event_loop()
-
-    x = [0]
-
-    # async def counter():
-    #    while True:
-    #        await asyncio.sleep(0.001)
-    #        x[0] += 1
-
-    await identity.init()
-    task = loop.create_task(counter())
-    await identity.generate_keys()
-    task.cancel()
-
-    # Make sure the inner loop of counter ran more than 20 times, which
-    # assumes that key generation took more than 20 ms
-    assert x[0] > 20
-
-
-@pytest.mark.slow
 async def test_signing(working_dir):
     "test our sign and verify functions"
 
