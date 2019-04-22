f47d5bbb782404ceeb29f8bdef78f566f91116b2
Hannes Gräuler <hannes@smasi.de>
2019-04-22 13:18:27 +0200

    Fix check-update

--- syncrypt/utils/updates.py
+++ syncrypt/utils/updates.py
@@ -1,10 +1,11 @@
+import json
 import ssl
 from distutils.version import LooseVersion  # pylint: disable=import-error,no-name-in-module
 
-import aiohttp
 import certifi
 
 import syncrypt
+from syncrypt.pipes import URLReader
 
 # The endpoint should return something along the lines of:
 # { "darwin": "x.y.z", "linux": "x.y.z", "win": "x.y.z" }
@@ -12,12 +13,8 @@ CURRENT_ENDPOINT = 'https://alpha.syncrypt.space/releases/current.json'
 
 
 async def retrieve_available_version(platform_id):
-    sslcontext = ssl.create_default_context(cafile=certifi.where())
-    conn = aiohttp.TCPConnector(ssl_context=sslcontext)
-    async with aiohttp.ClientSession(connector=conn) as c:
-        r = await c.get(CURRENT_ENDPOINT)
-        content = await r.json()
-        return content[platform_id]
+    content = await URLReader(CURRENT_ENDPOINT).readall()
+    return json.loads(content)[platform_id]
 
 
 async def is_update_available():
