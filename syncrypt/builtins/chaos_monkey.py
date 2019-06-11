611cd27d1a5d087d94e37dabbca87bc0a1d561b4
Hannes Gräuler <hannes@smasi.de>
2019-06-11 08:53:43 +0200

    Port chaos monkey to trio

--- syncrypt/builtins/chaos_monkey.py
+++ syncrypt/builtins/chaos_monkey.py
@@ -2,15 +2,15 @@
 This builtin module implements a basic chaos monkey. It will randomly raise an exception in
 specific function calls to harden the code.
 """
-
-import asyncio
 import inspect
 import logging
 import random
-from typing import List, Tuple, Union, Any # pylint: disable=unused-import
+import re
 import sys
+from typing import Any, List, Tuple, Union  # pylint: disable=unused-import
 
 import smokesignal
+import trio
 
 logger = logging.getLogger(__name__)
 
@@ -18,6 +18,7 @@ CHAOS_TABLE = [
     # Function names, Probability, Exception
     (("write_term", "read_term"), 0.015, IOError),
     (("open_connection",), 0.1, IOError),
+    #((".*",), 0.0004, IOError),
 ]  # type: List[Tuple[Union[Tuple[str], Tuple[str,str]], float, Any]]
 
 
@@ -28,15 +29,17 @@ def chaos_trace(frame, event, arg):
         except AttributeError:
             return chaos_trace
         for (function_names, probability, exc) in CHAOS_TABLE:
-            if tb.function in function_names:
-                if random.random() < probability:
-                    logger.warning(
-                        "Chaos monkey triggered for %s @ %s:%s",
-                        tb.function,
-                        tb.filename,
-                        tb.lineno,
-                    )
-                    raise exc("Chaos Monkey at it")
+            for func_pattern in function_names:
+                #print(tb.function, re.match(func_pattern, tb.function))
+                if re.match(func_pattern, tb.function):
+                    if random.random() < probability:
+                        logger.warning(
+                            "Chaos monkey triggered for %s @ %s:%s",
+                            tb.function,
+                            tb.filename,
+                            tb.lineno,
+                        )
+                        raise exc("Chaos Monkey at it")
     return chaos_trace
 
 
@@ -49,12 +52,11 @@ async def settrace_periodically():
     while True:
         if sys.gettrace() != chaos_trace:
             sys.settrace(chaos_trace)
-        await asyncio.sleep(2)
+        await trio.sleep(2)
 
 
-@smokesignal.once("pre_setup")
+@smokesignal.on("pre_setup")
 def pre_setup(app):
     # random.seed(12345)
     sys.settrace(chaos_trace)
-    loop = asyncio.get_event_loop()
-    loop.create_task(settrace_periodically())
+    app.nursery.start_soon(settrace_periodically)
